c3f4aff7d890bee4df6d552f6b9bb9cab27a1fa0
Ashok Mamnani <ashok.mamnani01@gmail.com>
2019-08-29 03:38:03 +0530

    admin customization django

---
+++ Lecture 01 - Admin Panel/Demo 3 Change Order/my_video_rental/video/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+class MovieAdmin(admin.ModelAdmin):
+     fields=['release_year','title','length']
+     
+admin.site.register(models.Customer)
+admin.site.register(models.Movie,MovieAdmin)
+# Register your models here.
---
+++ Lecture 01 - Admin Panel/Demo 5 Adding Filter/my_video_rental/video/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import models
+
+class MovieAdmin(admin.ModelAdmin):
+     fields=['release_year','title','length']
+     search_fields=['title','length']
+     list_filter=['release_year','length']
+
+admin.site.register(models.Customer)
+admin.site.register(models.Movie,MovieAdmin)
+# Register your models here.
